f4db679db0a8eb3bc3e676a88d9ba433d4bce9e4
Martin Scavnicky <martin.scavnicky@gmail.com>
2018-07-21 04:47:48 -0400

    Support forcing quotes over literal terms.

--- js/literalize.js
+++ js/literalize.js
@@ -2,15 +2,15 @@ function transposeArray(array) {
   return _.zip.apply(_, array);
 }
 
-function toLiteral(term) {
+function toLiteral(term, options) {
   if (_.isArray(term)) {
     var strings = []
     for (var i = 0; i < term.length; i++) {
-      strings.push(toLiteral(term[i]));
+      strings.push(toLiteral(term[i], options));
     }
     return "[" + strings.join(', ') + "]";
   } else {
-    if (isNaN(term)) {
+    if (options.forceQuotes === true || isNaN(term)) {
       return "'" + term + "'";
     } else {
       return term;
@@ -18,7 +18,8 @@ function toLiteral(term) {
   }
 }
 
-function literalize(str, transpose) {
+function literalize(str, options) {
+  options = options || {};
   // XXX This is pretty bad hack. Papaparse does not support spaces by default.
   // We is also not able to override the defaul selection of guess delmiters.
   // There we remove the ASCII character 31 from the list and replace it with space.
@@ -28,12 +29,12 @@ function literalize(str, transpose) {
   // TODO Better error handling.
   var data = results.data;
   
-  if (transpose) {
+  if (options.transpose === true) {
     data = transposeArray(data);
   }
   // Unnest the 1D-arrays which were nested since parsing the CSV.
   if (_.isArray(data) && data.length == 1) {
     data = data[0];
   }
-  return toLiteral(data);
+  return toLiteral(data, options);
 }
